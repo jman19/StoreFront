b56c18f17faa2d48edc7ccb8650dc4263aa11362
jason <jasonguyw@gmail.com>
2019-08-04 22:24:58 -0400

    fixing config

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/{{your-app-name}}'));
 
